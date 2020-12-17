fa297f0dc091969b382e3b9bec43d4d0a1b6fa01
Carrie Willis <carriehwillisdev@gmail.com>
2020-12-16 19:47:41 -0500

    adding pages

---
+++ home.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <title>Home</title>
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+    <header>
+      <h1>Brody's Obama Fan Page</h1>
+    </header>
+    <div class="content">
+      <img src="obama.jpg" alt="Ooooobama">
+      <p class="quote">"Obama is the best person on the planet." -Brody
+      </p>
+      <p>This is a basic HTML website used to teach Brody coding. He will learn to create content with HTML, style it with CSS, and then push his code to production using GitHub Pages.</p>
+<a href="http://www.twitter.com/potus" title="Why feet pics? I have no idea, this kid is weird."><button>Click to see Trump's feet pics</button></a>
+<a href="http://www.twitter.com/barackobama" title="I'm telling you, I don't know what they're teaching these kids in school."><button>Barack Obama's feet pics</button></a>
+</div>
+  </body>
+</html>
